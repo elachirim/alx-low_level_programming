b4e63e3d94fc3e647dad9b47394ce54c9b4983f7
elachirim <elachirim1@gmail.com>
2023-06-15 09:47:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Returns: Always 0 (Success) 
+ */
+int main(void)
+{
+	Puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+} 
